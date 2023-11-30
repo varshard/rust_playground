318def84d32a6a0701e19357725054f271051400
Bhoomtawath <varshard0@gmail.com>
2023-11-30 22:43:30 +0700

    tree sum

--- crates/binary_tree/src/lib.rs
+++ crates/binary_tree/src/lib.rs
@@ -1,16 +1,20 @@
+// https://codereview.stackexchange.com/questions/266554/bst-implementation-in-rust
+use std::ops::Add;
+
 type TreeNode<T> = Box<Node<T>>;
+
 pub struct Tree<T> {
-    root: Option<TreeNode<T>>
+    root: Option<TreeNode<T>>,
 }
 
 #[derive(Debug)]
 pub struct Node<T> {
     value: T,
     left: Option<TreeNode<T>>,
-    right: Option<TreeNode<T>>
+    right: Option<TreeNode<T>>,
 }
 
-impl<T: PartialOrd> Node<T> {
+impl<T: Ord> Node<T> {
     fn new(value: T) -> Self {
         Node {
             value,
@@ -35,9 +39,9 @@ impl<T: PartialOrd> Node<T> {
     }
 }
 
-impl<T: PartialOrd> Tree<T> {
+impl<T: Ord + Add<Output = T> + Default + Copy> Tree<T> {
     fn new() -> Self {
-        Tree{ root: None }
+        Tree { root: None }
     }
 
     fn insert(&mut self, value: T) -> &mut Self {
@@ -49,6 +53,30 @@ impl<T: PartialOrd> Tree<T> {
         }
         self
     }
+
+    fn sum_tree(&self) -> T {
+        let mut sum: T = Default::default();
+        if self.root.is_none() {
+            sum
+        } else {
+            let mut stack = vec![self.root.as_ref().unwrap()];
+
+            while stack.len() > 0 {
+                if let Some(n) = stack.pop() {
+                    sum = sum + n.value;
+
+                    if n.left.is_some() {
+                        stack.push(n.left.as_ref().unwrap())
+                    }
+                    if n.right.is_some() {
+                        stack.push(n.right.as_ref().unwrap())
+                    }
+                }
+            }
+
+            sum
+        }
+    }
 }
 
 #[cfg(test)]
@@ -57,22 +85,23 @@ mod tests {
 
     #[test]
     fn initialize_tree() {
-       let tree : Tree<u32>= Tree::new();
+        let tree: Tree<u32> = Tree::new();
         assert!(tree.root.is_none())
     }
 
     #[test]
     fn insert_root() {
-        let mut tree : Tree<u32>= Tree::new();
+        let mut tree: Tree<u32> = Tree::new();
         assert!(tree.root.is_none());
 
         tree.insert(1);
-        assert_eq!(tree.root.unwrap().value, 1);
+        assert_eq!(tree.root.as_ref().unwrap().value, 1);
+        assert_eq!(tree.sum_tree(), 1);
     }
 
     #[test]
     fn insert_1_2() {
-        let mut tree : Tree<u32>= Tree::new();
+        let mut tree: Tree<u32> = Tree::new();
         tree.insert(1);
         tree.insert(2);
         // https://stackoverflow.com/a/25298034/927687
@@ -86,23 +115,25 @@ mod tests {
 
     #[test]
     fn insert_2_1_3() {
-        let mut tree : Tree<u32>= Tree::new();
+        let mut tree: Tree<u32> = Tree::new();
         tree.insert(2);
         tree.insert(1);
         tree.insert(3);
 
-        let root = tree.root.unwrap();
+        let root = tree.root.as_ref().unwrap();
 
         assert_eq!(root.value, 2);
         let right = root.right.as_ref().unwrap();
         assert_eq!(right.value, 3);
         let left = root.left.as_ref().unwrap();
         assert_eq!(left.value, 1);
+
+        assert_eq!(tree.sum_tree(), 6);
     }
 
     #[test]
     fn insert_1_2_3() {
-        let mut tree : Tree<u32>= Tree::new();
+        let mut tree: Tree<u32> = Tree::new();
         tree.insert(1);
         tree.insert(2);
         tree.insert(3);
