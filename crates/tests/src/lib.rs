02d70d07895f1204a69b2700aa1ca05180d80c6a
Bhoomtawath <varshard0@gmail.com>
2023-11-30 18:15:26 +0700

    coin change

--- crates/tests/src/lib.rs
+++ crates/tests/src/lib.rs
@@ -1,4 +1,20 @@
 pub fn add(left: usize, right: usize) -> usize {
-    left + right
+    match left.checked_add(right) {
+        Some(sum) => {
+            sum
+        }
+        None => {
+            usize::MAX
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn add_infinity() {
+        assert_eq!(add(usize::MAX, 1), usize::MAX)
+    }
 }
 
